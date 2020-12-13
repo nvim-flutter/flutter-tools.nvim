20f9b8fb0c58a298fd0a0a56f735068de03db063
Akin Sowemimo <akin.sowemimo@gmail.com>
2020-12-13 21:47:12 +0000

    Show error messages if unable to list devices

--- lua/flutter-tools/devices.lua
+++ lua/flutter-tools/devices.lua
@@ -28,50 +28,64 @@ function M.format(device)
   return " • " .. device.name .. " "
 end
 
-local function get_device(devices)
+---@param result table
+local function get_device(result)
   return function(_, data, _)
     for _, line in pairs(data) do
       local device = M.parse(line)
       if device then
-        table.insert(devices, device)
+        table.insert(result.devices, device)
       end
+      table.insert(result.data, line)
     end
   end
 end
 
----@param devices table list of devices
-local function show_devices(devices)
+local function setup_devices_win(result)
+  return function(buf, _)
+    if #result.devices > 0 then
+      api.nvim_buf_set_var(buf, "devices", result.devices)
+    end
+    api.nvim_buf_set_keymap(
+      buf,
+      "n",
+      "<CR>",
+      ":lua __flutter_tools_select_device()<CR>",
+      {silent = true, noremap = true}
+    )
+  end
+end
+
+---@param result table list of devices
+local function show_devices(result)
   return function(_, _, _)
     local formatted = {}
-    for _, item in pairs(devices) do
-      table.insert(formatted, M.format(item))
+    local has_devices = #result.devices > 0
+    if has_devices then
+      for _, item in pairs(result.devices) do
+        table.insert(formatted, M.format(item))
+      end
+    else
+      for _, item in pairs(result.data) do
+        table.insert(formatted, item)
+      end
     end
     if #formatted > 0 then
-      ui.popup_create(
-        "Flutter devices",
-        formatted,
-        function(buf, _)
-          api.nvim_buf_set_var(buf, "devices", devices)
-          api.nvim_buf_set_keymap(
-            buf,
-            "n",
-            "<CR>",
-            ":lua __flutter_tools_select_device()<CR>",
-            {silent = true, noremap = true}
-          )
-        end
-      )
+      ui.popup_create("Flutter devices", formatted, setup_devices_win(result))
     end
   end
 end
 
 function M.list()
-  local devices = {}
+  local result = {
+    data = {},
+    devices = {}
+  }
   jobstart(
     "flutter devices",
     {
-      on_stdout = get_device(devices),
-      on_exit = show_devices(devices),
+      on_stdout = get_device(result),
+      on_exit = show_devices(result),
       on_stderr = function(_, data, _)
         if data and data[1] ~= "" then
           utils.echomsg(data[1])
