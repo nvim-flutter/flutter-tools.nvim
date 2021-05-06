4b51ee5ff55a88db0e059269bfa38c1df7a931af
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-05-06 10:12:51 +0100

    fix: derive flutter sdk path for normal executables

--- lua/flutter-tools/executable.lua
+++ lua/flutter-tools/executable.lua
@@ -12,12 +12,47 @@ local M = {
 
 local dart_sdk = path.join("cache", "dart-sdk")
 
+local function _flutter_sdk_root(bin_path)
+  -- convert path/to/flutter/bin/flutter into path/to/flutter
+  return fn.fnamemodify(bin_path, ":h:h")
+end
+
+local function _dart_sdk_root(paths)
+  if paths.flutter_sdk then
+    -- On Linux installations with snap the dart SDK can be further nested inside a bin directory
+    -- so it's /bin/cache/dart-sdk whereas else where it is /cache/dart-sdk
+    local segments = { paths.flutter_sdk, "cache" }
+    if not path.is_dir(path.join(unpack(segments))) then
+      table.insert(segments, 2, "bin")
+    end
+    if path.is_dir(path.join(unpack(segments))) then
+      -- remove the /cache/ directory as it's already part of the SDK path above
+      segments[#segments] = nil
+      return path.join(unpack(vim.tbl_flatten({ segments, dart_sdk })))
+    end
+  end
+
+  if utils.executable("flutter") then
+    local flutter_path = fn.resolve(fn.exepath("flutter"))
+    local flutter_bin = fn.fnamemodify(flutter_path, ":h")
+    return path.join(flutter_bin, dart_sdk)
+  end
+
+  if utils.executable("dart") then
+    return fn.resolve(fn.exepath("dart"))
+  end
+
+  return ""
+end
+
 ---Get paths for flutter and dart based on the binary locations
 ---@return table<string, string>
 local function get_default_binaries()
+  local flutter_bin = fn.resolve(fn.exepath("flutter"))
   return {
-    flutter_bin = fn.resolve(fn.exepath("flutter")),
+    flutter_bin = flutter_bin,
     dart_bin = fn.resolve(fn.exepath("dart")),
+    flutter_sdk = _flutter_sdk_root(flutter_bin),
   }
 end
 
@@ -55,34 +90,6 @@ local function path_from_lookup_cmd(lookup_cmd, callback)
   job:start()
 end
 
-local function _dart_sdk_root(paths)
-  if paths.flutter_sdk then
-    -- On Linux installations with snap the dart SDK can be further nested inside a bin directory
-    -- so it's /bin/cache/dart-sdk whereas else where it is /cache/dart-sdk
-    local segments = { paths.flutter_sdk, "cache" }
-    if not path.is_dir(path.join(unpack(segments))) then
-      table.insert(segments, 2, "bin")
-    end
-    if path.is_dir(path.join(unpack(segments))) then
-      -- remove the /cache/ directory as it's already part of the SDK path above
-      segments[#segments] = nil
-      return path.join(unpack(vim.tbl_flatten({ segments, dart_sdk })))
-    end
-  end
-
-  if utils.executable("flutter") then
-    local flutter_path = fn.resolve(fn.exepath("flutter"))
-    local flutter_bin = fn.fnamemodify(flutter_path, ":h")
-    return path.join(flutter_bin, dart_sdk)
-  end
-
-  if utils.executable("dart") then
-    return fn.resolve(fn.exepath("dart"))
-  end
-
-  return ""
-end
-
 ---Fetch the paths to the users binaries.
 ---@param callback fun(paths: table<string, string>)
 ---@return nil
@@ -95,8 +102,7 @@ function M.get(callback)
   if conf.flutter_path then
     _paths = {
       flutter_bin = conf.flutter_path,
-      -- convert path/to/flutter/bin/flutter into path/to/flutter
-      flutter_sdk = fn.fnamemodify(conf.flutter_path, ":h:h"),
+      flutter_sdk = _flutter_sdk_root(conf.flutter_path),
     }
     _paths.dart_sdk = _dart_sdk_root(_paths)
     return callback(_paths)
