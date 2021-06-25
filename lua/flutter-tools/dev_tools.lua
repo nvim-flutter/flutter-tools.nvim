14cbc4a957399a2172a6354575b2103def50c3af
Carlos Javier Gutiérrez Sánchez <13708100+Charly6596@users.noreply.github.com>
2021-06-25 16:08:37 +0200

    Update lua/flutter-tools/dev_tools.lua
    Co-authored-by: Akin <akin.sowemimo@gmail.com>

--- lua/flutter-tools/dev_tools.lua
+++ lua/flutter-tools/dev_tools.lua
@@ -44,7 +44,7 @@ local function start_browser()
   if not auto_open_browser then return end
   local url = M.get_profiler_url()
   if url then
-    local open_cmd = vim.loop.os_uname() == "Darwin" and "open" or "xdg-open"
+    local open_cmd = vim.loop.os_uname().sysname == "Darwin" and "open" or "xdg-open"
     vim.fn.jobstart({open_cmd, url}, {detach = true})
   end
 end
