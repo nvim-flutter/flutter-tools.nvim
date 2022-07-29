3dfe94cec788e274178bbd0e872d2ab660bf9e59
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-07-29 14:35:24 +0300

    feat: prevent writes to files in pub cache and FVM folder (#173)

--- ftplugin/dart/init.lua
+++ ftplugin/dart/init.lua
@@ -2,3 +2,13 @@ require("flutter-tools.lsp").attach()
 
 vim.opt_local.comments = [[sO:*\ -,mO:*\ \ ,exO:*/,s1:/*,mb:*,ex:*/,:///,://]]
 vim.opt_local.commentstring = [[//%s]]
+
+local full_path = vim.fn.expand("%:p")
+-- Prevent writes to files in the pub cache and FVM folder.
+if
+  full_path:find([[.pub-cache]])
+  or full_path:find([[Pub\Cache]])
+  or full_path:find([[/fvm/versions/]])
+then
+  vim.opt_local.modifiable = false
+end
