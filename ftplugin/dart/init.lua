165e4c7ef02e932e9a02b778266740687714e1dd
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-07-29 11:38:45 +0300

    fixup! feat: set dart comments options

--- ftplugin/dart/init.lua
+++ ftplugin/dart/init.lua
@@ -1,4 +1,4 @@
 require("flutter-tools.lsp").attach()
 
-vim.api.nvim_buf_set_option(0, "comments", [[sO:*\ -,mO:*\ \ ,exO:*/,s1:/*,mb:*,ex:*/,:///,://]])
-vim.api.nvim_buf_set_option(0, "commentstring", [[//%s]])
+vim.opt_local.comments = [[sO:*\ -,mO:*\ \ ,exO:*/,s1:/*,mb:*,ex:*/,:///,://]]
+vim.opt_local.commentstring = [[//%s]]
