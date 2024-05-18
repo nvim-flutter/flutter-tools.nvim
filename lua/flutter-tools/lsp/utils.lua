c19f94576f866888f1b84aa73c690b30de4b86fb
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2024-05-18 13:51:08 +0300

    fix: replace deprecated vim.lsp.get_active_clients with  vim.lsp.get_clients

--- lua/flutter-tools/lsp/utils.lua
+++ lua/flutter-tools/lsp/utils.lua
@@ -4,10 +4,11 @@ local lsp = vim.lsp
 
 M.SERVER_NAME = "dartls"
 
+-- TODO: Remove after compatibility with Neovim=0.9 is dropped
+local get_clients = vim.fn.has("nvim-0.10") == 1 and lsp.get_clients or lsp.get_active_clients
+
 ---@param bufnr number?
----@return lsp.Client?
-function M.get_dartls_client(bufnr)
-  return lsp.get_active_clients({ name = M.SERVER_NAME, bufnr = bufnr })[1]
-end
+---@return vim.lsp.Client?
+function M.get_dartls_client(bufnr) return get_clients({ name = M.SERVER_NAME, bufnr = bufnr })[1] end
 
 return M
